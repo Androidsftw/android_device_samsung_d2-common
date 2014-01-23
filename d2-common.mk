d9ca3666276260c5c536caf4b4b30464f8611ed3
Androidsftw <androidsftwsub@gmail.com>
2014-01-22 22:24:59 -0700

    my cherry-picks fix init, no need to copy a working one anymore

--- d2-common.mk
+++ d2-common.mk
@@ -75,10 +75,6 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
-#working init binary
-PRODUCT_COPY_FILES += \
-    device/samsung/d2-common/rootdir/etc/init:root/init
-
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8960
